6fefd91a79f147b0dce944f1517d0090574ca229
trangiahuy1477 <trgiahuy2906@gmail.com>
2024-10-25 21:36:52 +0700

    Add files via upload

---
+++ src/java/model/Coupon.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Coupon {
+    private int couponId;
+    private String couponName;
+    private String couponCode; 
+    private double discountValue;
+    private int quantity;
+    
+    public Coupon() {
+    }
+    
+    public Coupon(int couponId, String couponName, String couponCode, double discountValue, int quantity) {
+        this.couponId = couponId;
+        this.couponName = couponName;
+        this.couponCode = couponCode;
+        this.discountValue = discountValue;
+        this.quantity = quantity;
+    }
+    
+    // Getters and setters
+    public int getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponId(int couponId) {
+        this.couponId = couponId;
+    }
+
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
+
+    public String getCouponCode() {
+        return couponCode;
+    }
+
+    public void setCouponCode(String couponCode) {
+        this.couponCode = couponCode;
+    }
+
+    public double getDiscountValue() {
+        return discountValue;
+    }
+
+    public void setDiscountValue(double discountValue) {
+        this.discountValue = discountValue;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
